1d6c47fee7aee56c89c3db7d065b99a93c2844da
cabuke0 <s6egcabu@uni-bonn.de>
2024-12-01 22:18:11 +0100

    improve imports in init

--- explainability_toolkit/__init__.py
+++ explainability_toolkit/__init__.py
@@ -1,13 +1,63 @@
-from .boolean_functions_3x3 import *
-from .data_generation import *
-from .image_loader import *
-from .models import *
+"""
+Explainability Toolkit for analyzing machine learning models
+on boolean functions with 3x3 grid inputs.
+"""
+
+from .boolean_functions_3x3 import (
+    first_example,
+    min_x_amount_equals_one,
+    x_many_consecutively_one
+)
+from .data_generation import (
+    generate_complete_dataset,
+    split_dataset,
+    generate_sampled_datasets,
+    prepare_data
+)
+from .models import (
+    SimpleNet,
+    ImprovedCNN
+)
+from .train import (
+    decision_tree,
+    train_fully_connected,
+    train_cnn,
+    SEED_LIST,
+    ST_NUM_EPOCHS
+)
+from .utils import (
+    archive_old_results,
+    save_predictions,
+    vector_to_string
+)
 
 __all__ = [
+    # Boolean functions
     "first_example",
     "min_x_amount_equals_one",
     "x_many_consecutively_one",
-    "generate_disjoint_datasets",
-    "split_images_to_sets",
-    "decision_tree"
+    
+    # Data generation
+    "generate_complete_dataset",
+    "split_dataset",
+    "generate_sampled_datasets",
+    "prepare_data",
+    
+    # Models
+    "SimpleNet",
+    "ImprovedCNN",
+    
+    # Training
+    "decision_tree",
+    "train_fully_connected",
+    "train_cnn",
+    "SEED_LIST",
+    "ST_NUM_EPOCHS",
+    
+    # Utilities
+    "archive_old_results",
+    "save_predictions",
+    "vector_to_string"
 ]
+
+__version__ = "0.1.0"
